0ca17d34ecd42811c3cb3487cbb94d8e27fb6027
annasfe <anna_sfe@hotmail.com>
2023-05-14 22:48:22 +0300

    comments and steps

--- routes/users.js
+++ routes/users.js
@@ -13,19 +13,30 @@ const supersecret = process.env.SUPER_SECRET;
 /********* REGISTER - done through postman, no frontend form provided *********/
 
 router.post("/register", async (req, res) => {
-  
+   //0. get user info from request body
+   //1. encrypt password (⇒ `bcrypt.hash()`)
+   //2. create new user on DB to store user credentials
+   //3. respond with ok
 });
 
 
 /*********  LOGIN  *********/
 
 router.post("/login", async (req, res) => {
-  
+   //1. check if user exists on DB
+   //2. check if pwd correct (compare passwords ⇒ `bcrypt.compare()`)
+   //3. create token using user id (⇒ `sign()`)
+   //4. respond with token
 });
+
+
 
 /*********  PRIVATE ROUTE FOR LOGGED IN USERS ONLY *********/
 router.get("/private", (req, res) => {
 
+  //1. check if user logged in ⇒ get token from header and check it (⇒ `verify()`)
+  //2. get from token payload the user id to know which user is logged in
+  //3. respond requested data for specific user
 });
 
 module.exports = router;
