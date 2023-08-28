d1c6423b6e7ab3d9df67e24a966cca11c49128ff
annasfe <anna_sfe@hotmail.com>
2023-08-28 10:16:31 +0300

    cleaned up starter routes

--- routes/users.js
+++ routes/users.js
@@ -15,21 +15,19 @@ const supersecret = process.env.SUPER_SECRET;
 /** all paths start with /api/users  **/
 
 
-/********* REGISTER - done through postman, no frontend form provided *********/
+/********* REGISTER  *********/
 
 router.post("/register", async (req, res) => {
    //0. get user info from request body
-   const {name, email, username, password} = req.body;
 
    try {
    //1. encrypt password (⇒ `bcrypt.hash()`)
-   const hashedPwd = await bcrypt.hash(password, saltRounds);
-   console.log(hashedPwd);
+
    //2. create new user on DB to store user credentials
-   await db (`INSERT into users (name, email, username, password) VALUES ("${name}", "${email}", "${username}", "${hashedPwd}")`);
+
    //3. respond with ok
-   res.status(200).send({message: "Registration successful"});
-   } catch (err) {
+
+} catch (err) {
       res.status(400).send(err);
    }
 
@@ -39,77 +37,53 @@ router.post("/register", async (req, res) => {
 /*********  LOGIN  *********/
 
 router.post("/login", async (req, res) => {
-   const {username, password} = req.body;
+   //0. get user info from request body
 
    try {
    //1. check if user exists on DB
-      const results = await db(`SELECT * FROM users WHERE username = "${username}"`);
-      //SQL query returns an array, our user should be the first item
-      const user = results.data[0];
+      //hint: SQL query returns an array, our user should be the first item
+
+   //if user found...   
    if (user) {
       //2. check if pwd correct (compare passwords ⇒ `bcrypt.compare()`)
-      const isCorrect = await bcrypt.compare(password, user.password);
-      if(!isCorrect) res.status(401).send({message: "Password not correct"});
 
-      //3. create token using user id (⇒ `sign()`)
-      let payload = {userID: user.id};
-      const token = jwt.sign(payload, supersecret);
+      //3.1 if not correct send back error message
+
+      //3.2 if correct create token using user id (⇒ `sign()`)
 
       //4. respond with token
-      res.status(200).send({token: token});
+   
+   //if no user found... 
    } else {
       res.status(401).send({message: "User not found"});
    }
-   } catch(err) {
+
+} catch(err) {
       res.status(400).send(err);
    }
 
 });
 
 
-// MIDDLEWARE - GUARD FUNCTION 
-function userIsLoggedIn(req, res, next) {
+/*********  PRIVATE ROUTE FOR LOGGED IN USERS ONLY *********/
+router.get("/private", async (req, res) => {
+
   //1. check if user logged in by extracting token
+
   // check "authorization" header, it has the format: "Bearer <token>"
   // and split the string to get only the <token> part
   let authHeader = req.headers["authorization"];
-  let arrayHeader = authHeader.split(" "); 
-  let token = arrayHeader[1];
 
-  //2. get from token payload the user id to know which user is logged in
   try {
-    // remember, payload includes the user_id we added to it when we created the token
-    let payload = jwt.verify(token, supersecret);
-    
-    //get from the payload the user_id and store in the req so we can use later
-    req.userID = payload.userID;
-    //call next so that we go to the next function in the chain
-    next();
-  }catch(err) {
-    //respond with the error and DON'T CALL next, so that the communication stops here
-    res.status(401).send({ error: "Wrong token, unauthorized user" });
-  }
-}
-
-
-/*********  PRIVATE ROUTE FOR LOGGED IN USERS ONLY *********/
-router.get("/private", userIsLoggedIn, async (req, res) => {
-  //FIRST TWO STEPS ARE MOVED TO THE MIDDLEWARE FUNCTION userIsLoggedIn
-  //1. check if user is logged in
-  //2. get userID from token
-  
-  //I know user is logged in because the userIsLoggedIn guard did the check
-  //and the userId was stored inside the req.userID
+  //2. extract from token payload the user id (to identify logged in user)
   
   //3. respond requested data for specific user
-  try {
-    let results = await db(`SELECT * from users WHERE id = ${req.userID}`);
-    console.log(results.data[0]);
-    res.send(results.data[0]);
-  }catch(error) {
+
+
+}catch(error) {
     res.status(500).send({message: "error"});
   }
-  
+   
 });
 
 module.exports = router;
