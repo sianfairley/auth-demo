ce05843c9b0755f8cedabb0809bb2aa0d98d2da0
annasfe <anna_sfe@hotmail.com>
2023-08-30 10:25:39 +0300

    cleaned frontend

--- client/src/components/UserView.jsx
+++ client/src/components/UserView.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function UserView() {
   const [privateData, setprivateData] = useState(null);
 
-  // get private data on page load -> useEffect()!
+  // get private user data on page load -> useEffect()!
   useEffect(()=>{
     requestData();
   }, [])
@@ -13,33 +13,24 @@ function UserView() {
   const navigate = useNavigate();
 
   const requestData = async () => {
-    //if no token then redirect to login, don't let the user see the page
-    if(!localStorage.getItem("token")) navigate("/");
+    //0. if no token then redirect to login, don't let the user see the page
 
     //1. send post request including authorization header
-    let options = {
-      method: "GET",
-      headers: { "authorization": "Bearer " + localStorage.getItem("token")}
-    }
-    let response = await fetch("/api/users/private", options);
-    let data = await response.json();
     
     //2. store response private data
-    setprivateData(data);
-  
   };
 
   const logout = () => {
     //do the logout
+
     //1. remove token from local storage
-    localStorage.removeItem("token");
+
     //2. redirect to login page
-    navigate("/");
   };
 
   return (
     <div>
-      {privateData?.name && (
+      {privateData && (
         <div>
           {privateData.name} {privateData.email}
         </div>
